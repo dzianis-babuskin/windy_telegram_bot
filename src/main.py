7959cb735d1788c6faa93c72e2193f3610fae4ba
Dzianis Babuskin <3169477@gmail.com>
2020-05-06 18:33:18 +0300

    wind speed for any city by name

--- src/main.py
+++ src/main.py
@@ -1,22 +1,23 @@
 import datetime
 
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 from src.const import BOT_TOKEN
 from src.open_weather import get_hourly_weather_by_name
 
 
 def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!\nSend /help for info.")
 
 def help(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Send '/wind' to bot to know the wind speed for upcoming hours")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Send '/{City name}' to bot to know the wind speed for upcoming hours")
 
-def wind(update, context, city_name='Minsk'):
-    weather_json = get_hourly_weather_by_name(city_name)
+def wind(update, context):
+    city_name = update.message.text[1:]
+    weather_json = get_hourly_weather_by_name(city_name) if city_name else get_hourly_weather_by_name('minsk')
     weather_string = 'Remind you that Mavic mini max wind resistance is 8 m/s\n'
-    for weather in weather_json:
-        weather_string += f'Wind speed at {time(get_timestamp_from_json(weather))} - {get_wind_speed_from_json(weather):.1f} m/s'
+    for hour, weather in enumerate(weather_json):
+        weather_string += f'Wind speed in {hour} hours - {get_wind_speed_from_json(weather):.1f} m/s'
         weather_string += '\n'
     context.bot.send_message(chat_id=update.effective_chat.id, text=weather_string)
 
@@ -25,7 +26,7 @@ def main():
     dp = updater.dispatcher
     dp.add_handler(CommandHandler('start', start))
     dp.add_handler(CommandHandler('help', help))
-    dp.add_handler(CommandHandler('wind', wind))
+    dp.add_handler(MessageHandler(Filters.text, wind))
     updater.start_polling()
     updater.idle()
 
